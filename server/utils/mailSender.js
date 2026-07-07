a2996cb4c4b81c7fe1fa5a988844f639c6b37d2b
anasshaikh786 <84125169+anasshaikh786@users.noreply.github.com>
2026-07-07 08:20:57 +0530

    Fail fast on mail delivery errors

--- server/utils/mailSender.js
+++ server/utils/mailSender.js
@@ -1,28 +1,35 @@
 const nodemailer = require("nodemailer")
 
 const mailSender = async (email, title, body) => {
-  try {
-    let transporter = nodemailer.createTransport({
-      host: process.env.MAIL_HOST,
-      auth: {
-        user: process.env.MAIL_USER,
-        pass: process.env.MAIL_PASS,
-      },
-      secure: false,
-    })
+  const { MAIL_HOST, MAIL_USER, MAIL_PASS, MAIL_PORT, MAIL_SECURE } =
+    process.env
 
-    let info = await transporter.sendMail({
-      from: `"SkillBridge" <${process.env.MAIL_USER}>`, // sender address
-      to: `${email}`, // list of receivers
-      subject: `${title}`, // Subject line
-      html: `${body}`, // html body
-    })
-    console.log(info.response)
-    return info
-  } catch (error) {
-    console.log(error.message)
-    return error.message
+  if (!MAIL_HOST || !MAIL_USER || !MAIL_PASS) {
+    throw new Error("Mail service is not configured")
   }
+
+  const transporter = nodemailer.createTransport({
+    host: MAIL_HOST,
+    port: Number(MAIL_PORT) || 587,
+    secure: MAIL_SECURE === "true",
+    auth: {
+      user: MAIL_USER,
+      pass: MAIL_PASS,
+    },
+    connectionTimeout: 10000,
+    greetingTimeout: 10000,
+    socketTimeout: 15000,
+  })
+
+  const info = await transporter.sendMail({
+    from: `"SkillBridge" <${MAIL_USER}>`,
+    to: email,
+    subject: title,
+    html: body,
+  })
+
+  console.log(info.response)
+  return info
 }
 
 module.exports = mailSender
